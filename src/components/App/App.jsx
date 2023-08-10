63d86cb27715105c173da89011dfcdb81acc2dcd
Brie Morris <morris.brie@gmail.com>
2023-08-10 17:40:00 -0500

    class notes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,36 @@
 import './App.css';
+import React from 'react';
 
+const useState = React.useState;
 function App() {
+
+    // const [myStateVariable, setMyStateVariable] = useState('Initial Value')
+
+    // let whatUseStatereturns = useState(false);
+    // let myThing = whatUseStatereturns[0];
+    // let changeMyThing = whatUseStatereturns[1];
+
+    const [name, setName] = useState('Alex');
+    const [count, setCount] = useState(0);
+
+    const handleClick = () => {
+        console.log('clicked');
+        setCount(count + 1)
+    }
+
+    const decreaseClick = () => {
+        console.log('clicked');
+        setCount(count -1)
+    }
+
     return (
         <div className="App">
-            <p>Hello! My name is Luke.</p>
-            <button>Click me!</button>
-            <p>I've clicked the button 0 times.</p>
+            <p>Hello! My name is {name}.</p>
+            {/* <button onClick={() => console.log('clicked')} >Click me!</button> 
+            <button onClick = {() => setCount(count + 1)} >Click me!</button>*/}
+            <button onClick={handleClick}>Increase</button>
+            <button onClick={decreaseClick}>Decrease</button>
+            <p>I've clicked the button {count} times.</p>
         </div>
     );
 }
