06be347c85afc3b0a97911da5d819ee8cd5237c4
Brie Morris <morris.brie@gmail.com>
2023-08-10 18:01:46 -0500

    finsihed notes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -12,19 +12,30 @@ function App() {
 
     const [name, setName] = useState('Alex');
     const [count, setCount] = useState(0);
+    const [nameInput, setNameInput] = useState('');
 
     const handleClick = () => {
         console.log('clicked');
         setCount(count + 1)
+
     }
 
     const decreaseClick = () => {
         console.log('clicked');
-        setCount(count -1)
+        setCount(count -1);
+    }
+
+    const handleSubmit = () => {
+        setName(nameInput);
+        setNameInput('')
     }
 
     return (
         <div className="App">
+            {/* <input onChange={() => console.log('name changed!')} placeholder="name"/> */}
+            {/* <input onChange={(event) => console.log('name changed!', event.target.value)} placeholder="name"/> */}
+            <input value={nameInput} onChange={(event) => setNameInput(event.target.value)} placeholder="name"/>
+            <button onClick={handleSubmit}>Submit</button>
             <p>Hello! My name is {name}.</p>
             {/* <button onClick={() => console.log('clicked')} >Click me!</button> 
             <button onClick = {() => setCount(count + 1)} >Click me!</button>*/}
